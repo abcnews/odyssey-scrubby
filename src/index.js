810ea724ed6b2d95eb6640da5abde46ec89f7fa5
Colin Gourlay <gourlay.colin@abc.net.au>
2020-02-06 10:21:59 +1000

    Add example that mocks out Odyssey document

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from "./components/App";
 
 const configs = [];
 
+const MARKER_NAME = "scrubby";
+
 function render() {
   configs.forEach(({ rootEl, props }) => {
     rootEl.removeChild(rootEl.firstChild);
@@ -12,56 +14,60 @@ function render() {
 }
 
 function init() {
-  [...document.querySelectorAll(`a[name^="scrubby"]`)].forEach(markerEl => {
-    const blockEl = markerEl.closest(".Block");
+  [...document.querySelectorAll(`a[name^="${MARKER_NAME}"]`)].forEach(
+    markerEl => {
+      const blockEl = markerEl.closest(".Block");
 
-    if (!blockEl) {
-      return;
-    }
-
-    const rootEl = blockEl.querySelector(".Block-media");
-
-    if (!rootEl) {
-      return;
-    }
-
-    const { id, start, end } = acto(markerEl.getAttribute("name").slice(7));
-
-    if (!id) {
-      return;
-    }
-
-    const dataAttr = `data-odyssey-scrubby-${id}`;
-
-    const dataEl = document.querySelector(`[${dataAttr}]`);
-
-    if (!dataEl) {
-      return;
-    }
-
-    let dataURL = dataEl.getAttribute(dataAttr);
-
-    if (!dataURL) {
-      return;
-    }
-
-    const portraitDataURL = dataEl.getAttribute(`${dataAttr}-portrait`);
-
-    if (portraitDataURL && window.innerWidth / window.innerHeight <= 0.75) {
-      dataURL = portraitDataURL;
-    }
-
-    markerEl.parentElement.parentElement.removeChild(markerEl.parentElement);
-
-    configs.push({
-      rootEl,
-      props: {
-        dataURL,
-        startVH: start || 0,
-        endVH: end || 0
+      if (!blockEl) {
+        return;
       }
-    });
-  });
+
+      const rootEl = blockEl.querySelector(".Block-media");
+
+      if (!rootEl) {
+        return;
+      }
+
+      const { id, start, end } = acto(
+        markerEl.getAttribute("name").slice(MARKER_NAME.length)
+      );
+
+      if (!id) {
+        return;
+      }
+
+      const dataAttr = `data-odyssey-scrubby-${id}`;
+
+      const dataEl = document.querySelector(`[${dataAttr}]`);
+
+      if (!dataEl) {
+        return;
+      }
+
+      let dataURL = dataEl.getAttribute(dataAttr);
+
+      if (!dataURL) {
+        return;
+      }
+
+      const portraitDataURL = dataEl.getAttribute(`${dataAttr}-portrait`);
+
+      if (portraitDataURL && window.innerWidth / window.innerHeight <= 0.75) {
+        dataURL = portraitDataURL;
+      }
+
+      markerEl.parentElement.parentElement.removeChild(markerEl.parentElement);
+
+      configs.push({
+        rootEl,
+        props: {
+          dataURL,
+          startVH: start || 0,
+          endVH: end || 0
+        }
+      });
+    }
+  );
 
   render();
 }
