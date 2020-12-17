512b5eb889ff244bf76006a0074b36c268aa3127
Colin Gourlay <gourlay.colin@abc.net.au>
2020-12-17 10:27:42 +1000

    Add mount-utils so we can support loading mount points across both Phase 1 and Presentation Layer. Add base-36-props in order to introduce mount point ENCODED lottie config URLs as an alternative to relying on data-*attributes in HTMLFragments. initAfterOtherOdysseyPlugins will wait on Odyssey-related Associated JS as well as init-interactive definitions.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import "./polyfills";
-import * as acto from "@abcnews/alternating-case-to-object";
-import App from "./components/App";
+import './polyfills';
+import * as acto from '@abcnews/alternating-case-to-object';
+import { decode } from '@abcnews/base-36-props';
+import { getMountValue, isMount, selectMounts } from '@abcnews/mount-utils';
+import App from './components/App';
 
 const configs = [];
 
-const MARKER_NAME = "scrubby";
+const MARKER_NAME = 'scrubby';
 
 function render() {
   configs.forEach(({ rootEl, props }) => {
@@ -14,68 +16,95 @@ function render() {
 }
 
 function init() {
-  [...document.querySelectorAll(`a[name^="${MARKER_NAME}"]`)].forEach(
-    markerEl => {
-      const blockEl = markerEl.closest(".Block");
+  selectMounts('scrubby').forEach(mountEl => {
+    const blockEl = mountEl.closest('.Block');
 
-      if (!blockEl) {
-        return;
+    if (!blockEl) {
+      return;
+    }
+
+    const rootEl = blockEl.querySelector('.Block-media');
+
+    if (!rootEl) {
+      return;
+    }
+
+    /*
+      dataURL (and optional portraitDataURL) can be defined in the page in one
+      of two ways:
+
+      1) An element with `data-odyssey-scrubby-{id}` and
+        `data-odyssey-scrubby-{id}-portrait` attributes, where {id} is the ID
+        prop on the #scrubby mount, e.g.:
+
+          <div data-mount id="scrubbyIDxyz"></div>
+          ...
+          <div
+            data-odyssey-scrubby-xyz="{...}/landscape/data.json"
+            data-odyssey-scrubby-xyz-portrait="{...}/portrait/data.json"
+          ></div>
+
+      2) A Base36 encoded object with dataURL & portraitDataURL props as the
+        ENCODED prop on the #scrubby mount, e.g.:
+
+          <div data-mount id="scrubbyENCODEDhfjghfds79ygfh3rgfhjgufds0g..."></div>
+
+    */
+
+    const { id, encoded, start, end } = acto(getMountValue(mountEl));
+
+    if (!id && !encoded) {
+      return;
+    }
+
+    let dataURL;
+    let portraitDataURL;
+
+    if (encoded) {
+      try {
+        ({ dataURL, portraitDataURL } = decode(encoded));
+      } catch (err) {
+        return console.error(err);
       }
-
-      const rootEl = blockEl.querySelector(".Block-media");
-
-      if (!rootEl) {
-        return;
-      }
-
-      const { id, start, end } = acto(
-        markerEl.getAttribute("name").slice(MARKER_NAME.length)
-      );
-
-      if (!id) {
-        return;
-      }
-
+    } else {
       const dataAttr = `data-odyssey-scrubby-${id}`;
-
       const dataEl = document.querySelector(`[${dataAttr}]`);
 
       if (!dataEl) {
         return;
       }
 
-      let dataURL = dataEl.getAttribute(dataAttr);
-
-      if (!dataURL) {
-        return;
-      }
-
-      const portraitDataURL = dataEl.getAttribute(`${dataAttr}-portrait`);
-
-      if (portraitDataURL && window.innerWidth / window.innerHeight <= 0.75) {
-        dataURL = portraitDataURL;
-      }
-
-      markerEl.parentElement.parentElement.removeChild(markerEl.parentElement);
-
-      configs.push({
-        rootEl,
-        props: {
-          dataURL,
-          startVH: start || 0,
-          endVH: end || 0
-        }
-      });
+      dataURL = dataEl.getAttribute(dataAttr);
+      portraitDataURL = dataEl.getAttribute(`${dataAttr}-portrait`);
     }
-  );
+
+    if (!dataURL) {
+      return;
+    }
+
+    if (portraitDataURL && window.innerWidth / window.innerHeight <= 0.75) {
+      dataURL = portraitDataURL;
+    }
+
+    mountEl.parentElement.parentElement.removeChild(mountEl.parentElement);
+
+    configs.push({
+      rootEl,
+      props: {
+        dataURL,
+        startVH: start || 0,
+        endVH: end || 0
+      }
+    });
+  });
 
   render();
 }
 
 function initAfterOtherOdysseyPlugins() {
   if (
-    Array.from(document.querySelectorAll(".init-interactive")).filter(x =>
-      x.getAttribute("data-scripts").match(/odyssey-/)
+    Array.from(document.querySelectorAll('script[src],.init-interactive')).filter(el =>
+      el.getAttribute(el.tagName === 'SCRIPT' ? 'src' : 'data-scripts').match(/odyssey-/)
     ).length
   ) {
     // Give other Odyssey plugins a couple of seconds to initialise,
@@ -90,15 +119,15 @@ function initAfterOtherOdysseyPlugins() {
 if (window.__ODYSSEY__) {
   initAfterOtherOdysseyPlugins();
 } else {
-  window.addEventListener("odyssey:api", initAfterOtherOdysseyPlugins);
+  window.addEventListener('odyssey:api', initAfterOtherOdysseyPlugins);
 }
 
 if (module.hot) {
-  module.hot.accept("./components/App", () => {
+  module.hot.accept('./components/App', () => {
     try {
       render();
     } catch (err) {
-      import("./components/ErrorBox").then(exports => {
+      import('./components/ErrorBox').then(exports => {
         const ErrorBox = exports.default;
         root.appendChild(new ErrorBox({ error: err }).el);
       });
@@ -106,6 +135,6 @@ if (module.hot) {
   });
 }
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   console.debug(`Public path: ${__webpack_public_path__}`);
 }
